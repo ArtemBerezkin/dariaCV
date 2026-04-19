2dab8803ace14c521bd95a23688ce554db20f350
Artem Berezkin <berezkingg@gmail.com>
2026-04-19 14:38:54 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from './components/Header/Header.jsx';
-import Intro from './components/Intro/Intro.jsx';
-import ICan from './components/ICan/ICan.jsx';
-import Portfolio from './components/Portfolio/Portfolio.jsx';
-import AboutMe from './components/AboutMe/AboutMe.jsx';
-import Footer from './components/Footer/Footer.jsx';
+import Header from './components/01_Header/Header.jsx';
+import Intro from './components/02_Intro/Intro.jsx';
+import ICan from './components/03_ICan/ICan.jsx';
+import Portfolio from './components/04_Portfolio/Portfolio.jsx';
+import AboutMe from './components/05_AboutMe/AboutMe.jsx';
+import Footer from './components/06_Footer/Footer.jsx';
 
 const App = () => (
   <>
